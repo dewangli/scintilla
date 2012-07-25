ae7544254dddc60ebcc7fef997b851e4f340eded
Amorph <amorph.dev@gmail.com>
2012-07-25 22:10:25 +0300

    Integrate 3.2.1

--- cocoa/PlatCocoa.mm
+++ cocoa/PlatCocoa.mm
@@ -1604,13 +1604,14 @@ void ListBoxImpl::Create(Window& /*parent*/, int /*ctrlID*/, Scintilla::Point pt
 
 void ListBoxImpl::SetFont(Font& font_)
 {
-  font.SetID(font_.GetID());
   // NSCell setFont takes an NSFont* rather than a CTFontRef but they
   // are the same thing toll-free bridged.
   QuartzTextStyle* style = reinterpret_cast<QuartzTextStyle*>(font_.GetID());
+  font.Release();
+  font.SetID(new QuartzTextStyle(*style));
   NSFont *pfont = (NSFont *)style->getFontRef();
   [[colText dataCell] setFont: pfont];
-  CGFloat itemHeight = lround([pfont ascender] - [pfont descender]);
+  CGFloat itemHeight = ceil([pfont boundingRectForFont].size.height);
   [table setRowHeight:itemHeight];
 }
 
